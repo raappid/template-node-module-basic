bf43230676b462655b80e71009a1d1d4dc653d9b
raappid <raappidgithub@gmail.com>
2016-02-17 20:16:27 -0700

    updating some files

--- scripts/clean.js
+++ scripts/clean.js
@@ -2,7 +2,15 @@
 
 var del = require('del');
 
-var paths = del.sync(['coverage','src/**/*.js','test/**/*.js','dist','**/*.map','!node_modules/**/*.map']);
+var paths = del.sync(['coverage',
+    'src/**/*.js',
+    'test/**/*.js',
+    'dist',
+    '**/*.map',
+    '!node_modules/**/*.map',
+    'src/**/*.d.ts',
+    'test/**/*.d.ts'
+    ]);
 
 
 console.log('Deleted files/folders:\n', paths.join('\n'));
