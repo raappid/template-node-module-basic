1ac0e37750e171a43d8f304c73b7c616c0267804
raappid <raappidgithub@gmail.com>
2016-12-05 22:18:49 -0700

    preparing for release

--- scripts/report-coverage.js
+++ scripts/report-coverage.js
@@ -1,15 +1,27 @@
 
 
 var exec = require('child_process').exec;
+var fs = require("fs");
+var path = require("path");
 
-exec("cat ./coverage/lcov.info | ./node_modules/.bin/coveralls", function(err, stdOut, stdErr){
-    console.log(stdOut);
-    console.log(stdErr);
-    if(err)
-    {
-        process.exit(1);
-    }
+if(fs.existsSync(path.resolve("./coverage")))
+{
+    exec("cat ./coverage/lcov.info | ./node_modules/.bin/coveralls", function(err, stdOut, stdErr){
+        console.log(stdOut);
+        console.log(stdErr);
+        if(err)
+        {
+            process.exit(1);
+        }
 
+        process.exit(0);
+    });
+}
+else
+{
+    //means no coverage report was generated
+    console.log("Nothing to Report: Coverage info not found");
     process.exit(0);
-});
+}
+
 
